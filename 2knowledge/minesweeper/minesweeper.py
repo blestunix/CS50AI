dfb83c577988b06aecbd31fb040cfbe9b4522919
blestunix <blessunix@gmail.com>
2021-12-20 17:56:37 +0530

    Game now gives valid outcome

--- 2knowledge/minesweeper/minesweeper.py
+++ 2knowledge/minesweeper/minesweeper.py
@@ -182,6 +182,7 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
+        self.safes.add(cell)
         
 
     def make_safe_move(self):
