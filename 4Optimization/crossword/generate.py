e6529640d79b737556df2fce52411c6a6e9c9451
blestunix <blessunix@gmail.com>
2022-01-06 15:27:22 +0530

    Implemented assignment completion check

--- 4Optimization/crossword/generate.py
+++ 4Optimization/crossword/generate.py
@@ -148,7 +148,7 @@ class CrosswordCreator():
         Return True if `assignment` is complete (i.e., assigns a value to each
         crossword variable); return False otherwise.
         """
-        raise NotImplementedError
+        return all(var in assignment for var in self.crossword.variables)
 
     def consistent(self, assignment):
         """
@@ -185,7 +185,9 @@ class CrosswordCreator():
 
         If no assignment is possible, return None.
         """
-        raise NotImplementedError
+        # Check if assignment is complete
+        if self.assignment_complete(assignment):
+            return assignment
 
 
 def main():
