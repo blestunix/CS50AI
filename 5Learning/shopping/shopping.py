bb222c1c30bc73f53c558dda1402ba63468488aa
blestunix <blessunix@gmail.com>
2022-01-08 20:33:32 +0530

    Implemented train_model()

--- 5Learning/shopping/shopping.py
+++ 5Learning/shopping/shopping.py
@@ -98,7 +98,12 @@ def train_model(evidence, labels):
     Given a list of evidence lists and a list of labels, return a
     fitted k-nearest neighbor model (k=1) trained on the data.
     """
-    raise NotImplementedError
+    model = KNeighborsClassifier(n_neighbors=1)
+
+    # Train the model
+    model.fit(X=evidence, y=labels)
+
+    return model
 
 
 def evaluate(labels, predictions):
