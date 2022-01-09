69a270db0b06f6600ddb6cb3ed098c0dc1c4ecca
blestunix <blessunix@gmail.com>
2022-01-09 16:03:05 +0530

    Revert "Implemented best_future_reward() method"
    This reverts commit 7b390de6caf4ced916709ed494ba1861ecb53f7e.

--- 5Learning/nim/nim.py
+++ 5Learning/nim/nim.py
@@ -121,7 +121,7 @@ class NimAI():
         `alpha` is the learning rate, and `new value estimate`
         is the sum of the current reward and estimated future rewards.
         """
-        self.q[(tuple(state), action)] = old_q + self.alpha * ((reward + future_rewards) - old_q)
+        raise NotImplementedError
 
     def best_future_reward(self, state):
         """
